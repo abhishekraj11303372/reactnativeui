8cc67dedfec7cc9f35273b048a561dbcc5bc079b
Abhishek Raj <abhishekraj1130@gmail.com>
2022-03-28 14:17:49 +0530

    init react native with libraries added

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+return(
+  <NativeContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="" component={UserLogin} options={{}} />
+    </Stack.Navigator>
+  </NativeContainer>
+); 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
