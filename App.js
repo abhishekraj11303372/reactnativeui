a9f4d9af07f223a8fa8fd175624f95fecf162c28
Abhishek Raj <abhishekraj1130@gmail.com>
2022-03-28 14:29:50 +0530

    one component added

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import { NativeContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import UserLoginScreen from './app/screen/auth/UserLoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 return(
-  <NativeContainer>
+  <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="" component={UserLogin} options={{}} />
+      <Stack.Screen name="UserLogin" component={UserLoginScreen} options={{}} />
     </Stack.Navigator>
-  </NativeContainer>
+  </NavigationContainer>
 ); 
 }
 
