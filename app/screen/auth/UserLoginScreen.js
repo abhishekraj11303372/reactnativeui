7f51253c42ae62ee8786cd30dab2ad59c2b29139
Abhishek Raj <abhishekraj1130@gmail.com>
2022-03-29 11:02:35 +0530

    login consoles message

--- app/screen/auth/UserLoginScreen.js
+++ app/screen/auth/UserLoginScreen.js
@@ -1,11 +1,38 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button, TextInput } from 'react-native'
+import React,{useState} from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { styles } from '../../../styles'
 
 const UserLoginScreen = () => {
+    const [email,SetEmail] = useState("");
+    const [password,SetPassword] = useState("");
+
+    const handleFormSubmit = () => {
+        if(email && password ) {
+            console.log("Login Success");
+        }
+        else {
+            console.log("All Fields are Required");
+        }
+    }
+
   return (
-    <View>
-      <Text>UserLoginScreen</Text>
-    </View>
+    <SafeAreaView>
+        <View>
+            <View style={{marginHorizontal:30,marginBottom:10}}>
+                <Text style={styles.labelText}>Email</Text>
+                <TextInput value={email} onChangeText={SetEmail} placeholder="Email here" onPress={console.log(email)} keyboardType='email-address' style={styles.input}/>
+            </View> 
+            <View style={{marginHorizontal:30}}>
+                <Text style={styles.labelText}>Password</Text>
+                <TextInput value={password} onChangeText={SetPassword} placeholder="Password here" onPress={console.log(password)} secureTextEntry={true} style={styles.input}/>
+            </View> 
+            <View style={{marginHorizontal:30,width:200,alignSelf:'center',margin:20}}>
+                <Button title='Login' onPress={handleFormSubmit} color='purple' />
+            </View> 
+            
+        </View>     
+    </SafeAreaView>
   )
 }
 
--- styles.js
+++ styles.js
@@ -1,8 +1,21 @@
-export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+import { StyleSheet,View, Text } from "react-native";
+
+const styles = StyleSheet.create({
+    labelText: {
+      marginBottom:5,
+      marginLeft:10,
+      fontSize:16,
+      fontWeight:'bold',
     },
+    input: {
+      height:40,
+      marginBottom:5,
+      marginLeft:10,
+      borderWidth:1,
+      borderRadius:5,
+      fontSize:15,
+      paddingLeft:15,
+    }
   });
+
+  export {styles};
