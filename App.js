36a0690b020a5880a0bb18737fd60cac32de46fa
Abhishek Raj <abhishekraj1130@gmail.com>
2022-04-09 17:24:27 +0530

    dashbooard init

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ return(
       <Stack.Screen name="UserLogin" component={UserLoginScreen} options={{title: 'User Login'}} />
       <Stack.Screen name="Registeration" component={RegistrationScreen} options={{title: 'Registeration',headerBackVisible:false}} />
       <Stack.Screen name="SendPasswordResetEmail" component={SendPasswordResetEmailScreen} options={{title: 'Password Password'}} />
-      <Stack.Screen name="UserPanelTab" component={UserPanelTabScreen} options={{title: 'User Panel'}} />
+      <Stack.Screen name="UserPanelTab" component={UserPanelTabScreen} options={{title: 'User Panel',headerShown:false}} />
     </Stack.Navigator>
   </NavigationContainer>
 ); 
--- app/screen/UserPanelTabScreen.js
+++ app/screen/UserPanelTabScreen.js
@@ -1,11 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import DashboardScreen from './DashboardScreen'
+import SideBar from './SideBar';
+
+const Drawer = createDrawerNavigator();
 
 const UserPanelTabScreen = () => {
   return (
-    <View>
-      <Text>UserPanelTabScreen</Text>
-    </View>
+    <Drawer.Navigator drawerContent={props => <SideBar {...props} />}>
+      <Drawer.Screen name='Dashboard' component={DashboardScreen} ></Drawer.Screen>
+      {/* <Drawer.Screen name='Change Password' component={} ></Drawer.Screen>
+      <Drawer.Screen name='About us' component={} ></Drawer.Screen> */}
+    </Drawer.Navigator>
   )
 }
 
