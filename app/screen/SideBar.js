df06c7fcec71b840ff53af84166a09c3e6b5552f
Abhishek Raj <abhishekraj1130@gmail.com>
2022-05-03 09:34:26 +0530

    slice to to store and unset

--- app/screen/SideBar.js
+++ app/screen/SideBar.js
@@ -4,9 +4,11 @@ import { useNavigation } from '@react-navigation/native'
 import { DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
 import { removeToken } from '../../services/AsyncStorageService';
 import { useSelector } from 'react-redux';
+import { unSetUserInfo } from '../features/userSlice';
 
 const SideBar = ({...props}) => {
     const handleLogout = async () => {
+      unSetUserInfo({ email:"",name:""})
       await removeToken('token');
         navigation.navigate('Home');
         // console.log("logout");
@@ -15,7 +17,9 @@ const SideBar = ({...props}) => {
     const navigation =useNavigation();
 //getting user data from redux store using useSelector
     const myData = useSelector(state => state.user)
+    const myToken = useSelector(state => state.auth)
     console.log("mydata",myData)
+    console.log("mytoken",myToken)
 
   return (
     <DrawerContentScrollView {...props}>
--- app/store.js
+++ app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { userAuthApi } from '../services/userAuthApi'
 import userReducer from '../app/features/userSlice'
+import authReducer from './features/authSlice'
 
 export const store = configureStore({
   reducer: {
     [userAuthApi.reducerPath]: userAuthApi.reducer,
-    user: userReducer
+    user: userReducer,
+    token: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(userAuthApi.middleware),
